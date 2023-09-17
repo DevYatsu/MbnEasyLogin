39cbf720cba442928613b5217354031c169d7f8d
Yatsu <yatsu.dev@gmail.com>
2023-09-17 18:42:51 +0200

    logo updated

--- src/manifest.ts
+++ src/manifest.ts
@@ -6,10 +6,9 @@ export default defineManifest({
   version: '1.0.0',
   manifest_version: 3,
   icons: {
-    '16': 'img/logo-16.png',
-    '32': 'img/logo-34.png',
-    '48': 'img/logo-48.png',
     '128': 'img/logo-128.png',
+    '48': 'img/logo-48.png',
+    '512': 'img/logo-512.png',
   },
   action: {
     default_popup: 'popup.html',
@@ -28,7 +27,7 @@ export default defineManifest({
   ],
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: ['img/logo-48.png', 'img/logo-128.png', 'img/logo-512.png'],
       matches: [],
     },
   ],

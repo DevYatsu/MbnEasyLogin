a28cea5afb8249498a6546d25796c11cc3916e0e
Yatsu <yatsu.dev@gmail.com>
2023-09-23 00:36:30 +0200

    compatibility issue removed

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,73 +1,79 @@
-const { isScriptRunner } = await chrome.runtime.sendMessage({
-  action: 'requestIsScriptRunner',
-})
+;(async () => {
+  const { isScriptRunner } = await chrome.runtime.sendMessage({
+    action: 'requestIsScriptRunner',
+  })
 
-if (isScriptRunner) {
-  let url = document.location.href
+  if (isScriptRunner) {
+    let url = document.location.href
 
-  if (
-    url ===
-    'https://cas.monbureaunumerique.fr/login?service=https%3A%2F%2Fwww.monbureaunumerique.fr%2Fsg.do%3FPROC%3DIDENTIFICATION_FRONT'
-  ) {
-    if (document.querySelector('.form__label[for="idp-EDU"]')) {
-      // Simulate a click on the element
-      ;(document.querySelector('.form__label[for="idp-EDU"]')! as HTMLAnchorElement).click()
-    }
-
-    if (document.getElementById('button-submit')) {
-      // Simulate a click on the element
-      document.getElementById('button-submit')?.click()
-    }
-  }
-
-  if (
-    url.startsWith('https://educonnect.education.gouv.fr/idp/profile/SAML2/POST/SSO?execution=')
-  ) {
     if (
-      !document.referrer.startsWith(
-        'https://educonnect.education.gouv.fr/idp/profile/SAML2/POST/SSO?execution=',
-      )
+      url ===
+      'https://cas.monbureaunumerique.fr/login?service=https%3A%2F%2Fwww.monbureaunumerique.fr%2Fsg.do%3FPROC%3DIDENTIFICATION_FRONT'
     ) {
-      document.getElementById('bouton_eleve')?.click()
+      if (document.querySelector('.form__label[for="idp-EDU"]')) {
+        // Simulate a click on the element
+        ;(document.querySelector('.form__label[for="idp-EDU"]')! as HTMLAnchorElement).click()
+      }
 
-      const { username, password } = await chrome.runtime.sendMessage({
-        action: 'requestCredentials',
+      if (document.getElementById('button-submit')) {
+        // Simulate a click on the element
+        document.getElementById('button-submit')?.click()
+      }
+    }
+
+    if (
+      url.startsWith('https://educonnect.education.gouv.fr/idp/profile/SAML2/POST/SSO?execution=')
+    ) {
+      if (
+        !document.referrer.startsWith(
+          'https://educonnect.education.gouv.fr/idp/profile/SAML2/POST/SSO?execution=',
+        )
+      ) {
+        document.getElementById('bouton_eleve')?.click()
+
+        const { username, password } = await chrome.runtime.sendMessage({
+          action: 'requestCredentials',
+        })
+
+        if (document.getElementById('username')) {
+          ;(document.getElementById('username')! as HTMLInputElement).value = username
+        }
+        if (document.getElementById('password')) {
+          ;(document.getElementById('password')! as HTMLInputElement).value = password
+        }
+
+        document.getElementById('bouton_valider')?.click()
+      }
+    }
+
+    if (url === 'https://cas.monbureaunumerique.fr/saml/SAMLAssertionConsumer') {
+      ;(
+        document.querySelector(
+          'div.msg__content > p:nth-child(4) > strong > a',
+        )! as HTMLAnchorElement
+      ).click()
+    }
+
+    // when arrived on the main app redirect to the first school in school list, which is certainly the school you go to
+    if (
+      document.location.href ===
+      'https://www.monbureaunumerique.fr/sg.do?PROC=PAGE_ACCUEIL&ACTION=VALIDER'
+    ) {
+      const { goToFirstSchoolAutomatically } = await chrome.runtime.sendMessage({
+        action: 'requestGoingOnFirstSchool',
       })
-
-      if (document.getElementById('username')) {
-        ;(document.getElementById('username')! as HTMLInputElement).value = username
-      }
-      if (document.getElementById('password')) {
-        ;(document.getElementById('password')! as HTMLInputElement).value = password
-      }
-
-      document.getElementById('bouton_valider')?.click()
-    }
-  }
-
-  if (url === 'https://cas.monbureaunumerique.fr/saml/SAMLAssertionConsumer') {
-    ;(
-      document.querySelector('div.msg__content > p:nth-child(4) > strong > a')! as HTMLAnchorElement
-    ).click()
-  }
-
-  // when arrived on the main app redirect to the first school in school list, which is certainly the school you go to
-  if (
-    document.location.href ===
-    'https://www.monbureaunumerique.fr/sg.do?PROC=PAGE_ACCUEIL&ACTION=VALIDER'
-  ) {
-    const { goToFirstSchoolAutomatically } = await chrome.runtime.sendMessage({
-      action: 'requestGoingOnFirstSchool',
-    })
-    if (goToFirstSchoolAutomatically) {
-      if (document.querySelector('nav > div > div > ul > li:nth-child(1) > a')) {
-        // go to first school in list
-        ;(
-          document.querySelector('nav > div > div > ul > li:nth-child(1) > a')! as HTMLAnchorElement
-        ).click()
+      if (goToFirstSchoolAutomatically) {
+        if (document.querySelector('nav > div > div > ul > li:nth-child(1) > a')) {
+          // go to first school in list
+          ;(
+            document.querySelector(
+              'nav > div > div > ul > li:nth-child(1) > a',
+            )! as HTMLAnchorElement
+          ).click()
+        }
       }
     }
   }
-}
+})()
 
 export {}

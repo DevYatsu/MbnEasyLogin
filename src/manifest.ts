faf360ce52ed0194c387faff1ac64652a8ba9587
Yatsu <yatsu.dev@gmail.com>
2024-02-07 20:26:44 +0100

    extension version updated

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   name: 'MbnEasyLogin',
   description: 'An extension to easily login to Mbn',
-  version: '1.0.0',
+  version: '1.0.2',
   manifest_version: 3,
   icons: {
     '128': 'img/logo-128.png',
